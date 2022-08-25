8258063aa6e5a637099aa5eea5ea1794b315a66d
PugswapFinance <111673538+PugswapFinance@users.noreply.github.com>
2022-08-25 12:01:46 +0700

    Update index.tsx

--- src/pages/SafelistContent/index.tsx
+++ src/pages/SafelistContent/index.tsx
@@ -21,70 +21,70 @@ const SafelistContent = () => {
 
     return (
         <Container>
-            <Title color={Theme.colors.primary}>{t('SOY Finance Safelisting')}</Title>
+            <Title color={Theme.colors.primary}>{t('Pug Inu Finance Safelisting')}</Title>
             <Spacer height="80px" />
             <TopDiv>
                 <SubTitle color={Theme.colors.bgscondary}>{t('What Exactly is The Safelist?')}</SubTitle>
                 <Spacer height="40px" />
                 <Text color={Theme.colors.primary}>{t('Security vulnerabilities are undoubtedly the biggest challenge for the DeFI ecosystem, with losses of hundreds of millions of dollars just for 2021.')}</Text>
                 <Spacer height="20px" />
-                <Text color={Theme.colors.primary}>{t('SOY Finance intends to offer the highest level of security to its users, and for this purpose, we have established the Safelist with the best security standards.')}</Text>
+                <Text color={Theme.colors.primary}>{t('Pug Inu Finance intends to offer the highest level of security to its users, and for this purpose, we have established the Safelist with the best security standards.')}</Text>
                 <Spacer height="40px" />
-                <SubTitle color={Theme.colors.bgscondary}>{t('How to Safelist a Token on SOY Finance?')}</SubTitle>
+                <SubTitle color={Theme.colors.bgscondary}>{t('How to Safelist a Token on Pug Inu Finance?')}</SubTitle>
                 <Spacer height="40px" />
-                <Text color={Theme.colors.primary}>{t('For project representatives and community members who wish to have their project token safelisted by SOY Finance, please follow the steps below:')}</Text>
+                <Text color={Theme.colors.primary}>{t('For project representatives and community members who wish to have their project token safelisted by Pug Inu Finance, please follow the steps below:')}</Text>
                 <Spacer height="40px" />
                 <Row>
                     <Circle />
-                    <Text color='#767676'>{t('The Callisto Security Department should have audited the token.')}</Text>
+                    <Text color='#767676'>{t('The ContractCheck Security Department should have audited the token.')}</Text>
                 </Row>
                 <Spacer height="20px" />
                 <Row>
                     <Circle />
-                    <Text color='#767676'>{t('Burn 10 000 SOY tokens by sending them to the')}&nbsp;&nbsp;<a href="https://explorer.callisto.network/address/0x000000000000000000000000000000000000dEaD/transactions" target="_blank" rel="noreferrer" style={{color: 'blue'}}>{t('"Burn address"')}.</a></Text>
+                    <Text color='#767676'>{t('Burn 40% PUG tokens by sending them to the')}&nbsp;&nbsp;<a href="https://bscscan.com/address/0x000000000000000000000000000000000000dead" target="_blank" rel="noreferrer" style={{color: 'blue'}}>{t('"Burn address"')}.</a></Text>
                 </Row>
             </TopDiv>
             <Spacer height="40px" />
             <BottomDiv>
                 <div>
-                    <Text color={Theme.colors.primary}>{t('Provide the following minimum liquidity:')}</Text>
+                    <Text color={Theme.colors.primary}>{t('')}</Text>
                     <Spacer height="40px" />
                     <Row2>
                         <Circle />
-                        <Text color='#767676'>{t('Ethereum: 50 000 USD.')}</Text>
+                        <Text color='#767676'>{t(')}</Text>
                     </Row2>
                     <Spacer height="20px" />
                     <Row2>
                         <Circle />
-                        <Text color='#767676'>{t('Callisto Network: 25 000 USD.')}</Text>
+                        <Text color='#767676'>{t('')}</Text>
                     </Row2>
                     <Spacer height="20px" />
                     <Row2>
                         <Circle />
-                        <Text color='#767676'>{t('Binance Smart Chain: 50 000 USD.')}</Text>
+                        <Text color='#767676'>{t('')}</Text>
                     </Row2>
                 </div>
                 <Div>
-                    <Text color={Theme.colors.primary}>{t('Send us the following information to')}<Text2 color={Theme.colors.bgscondary}><b>&nbsp;{t('Safelist@Soy.Finance.')}</b></Text2></Text>
+                    <Text color={Theme.colors.primary}>{t('Send us the following information to')}<Text2 color={Theme.colors.bgscondary}><b>&nbsp;{t('Pen@puginu.Finance.')}</b></Text2></Text>
                     <Spacer height="40px" />
                     <Row2>
                         <Circle />
-                        <Text color='#767676'>{t('The token’s description.')}</Text>
+                        <Text color='#767676'>{t('')}</Text>
                     </Row2>
                     <Spacer height="20px" />
                     <Row2>
                         <Circle />
-                        <Text color='#767676'>{t('The token’s smart contract address.')}</Text>
+                        <Text color='#767676'>{t('')}</Text>
                     </Row2>
                     <Spacer height="20px" />
                     <Row2>
                         <Circle />
-                        <Text color='#767676'>{t('The token’s logo ( SVG or PNG 256x256 ).')}</Text>
+                        <Text color='#767676'>{t('')}</Text>
                     </Row2>
                 </Div>
             </BottomDiv>
             <Spacer height="60px" />
-            <Text color={Theme.colors.primary}>{t('Tip: Mentioning your interest in SOY Finance on your social media is always appreciated.')}</Text>
+            <Text color={Theme.colors.primary}>{t('Tip: Mentioning your interest in Pug Inu Finance on your social media is always appreciated.')}</Text>
             <Spacer height="100px" />
         </Container>
     )
